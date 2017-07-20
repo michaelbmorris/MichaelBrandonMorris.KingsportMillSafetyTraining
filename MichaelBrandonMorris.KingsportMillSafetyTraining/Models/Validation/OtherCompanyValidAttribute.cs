672cb267665fd327b5f6865c8e694f55ebc54384
Michael Brandon Morris <brandon@michaelbmorris.com>
2017-07-20 14:31:30 -0400

    Started adding other company validation.

---
+++ MichaelBrandonMorris.KingsportMillSafetyTraining/Models/Validation/OtherCompanyValidAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Foolproof;
+using MichaelBrandonMorris.KingsportMillSafetyTraining.Models.Identity.Account;
+
+namespace MichaelBrandonMorris.KingsportMillSafetyTraining.Models.Validation
+{
+    public class OtherCompanyValidAttribute : ModelAwareValidationAttribute
+    {
+        public override bool IsValid(object value, object container)
+        {
+            var model = container as RegisterViewModel;
+
+            if (model == null)
+            {
+                throw new ArgumentException($"Attribute must be applied within a type of {typeof(RegisterViewModel)}");
+            }
+
+            // TODO Implement other company valid attribute
+            throw new NotImplementedException();
+        }
+    }
+}
