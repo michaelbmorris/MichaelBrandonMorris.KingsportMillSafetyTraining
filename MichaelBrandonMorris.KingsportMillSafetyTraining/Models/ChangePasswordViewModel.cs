bbfc0a7feb49dd005119e5243a3b16e17d0152b1
Michael Brandon Morris <brandon@michaelbmorris.com>
2017-08-18 14:27:37 -0400

    Modified change password to require verification.

--- MichaelBrandonMorris.KingsportMillSafetyTraining/Models/ChangePasswordViewModel.cs
+++ MichaelBrandonMorris.KingsportMillSafetyTraining/Models/ChangePasswordViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace MichaelBrandonMorris.KingsportMillSafetyTraining.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MichaelBrandonMorris.KingsportMillSafetyTraining.Models
 {
     /// <summary>
     ///     Class ChangePasswordViewModel.
@@ -6,6 +8,18 @@
     /// TODO Edit XML Comment Template for ChangePasswordViewModel
     public class ChangePasswordViewModel
     {
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare(
+            "Password",
+            ErrorMessage =
+                "The new password and confirmation password do not match.")]
+        public string ConfirmPassword
+        {
+            get;
+            set;
+        }
+
         /// <summary>
         ///     Gets or sets the email.
         /// </summary>
@@ -18,10 +32,17 @@
         }
 
         /// <summary>
-        ///     Gets or sets the password.
+        ///     Gets or sets the new password.
         /// </summary>
-        /// <value>The password.</value>
-        /// TODO Edit XML Comment Template for Password
+        /// <value>The new password.</value>
+        /// TODO Edit XML Comment Template for NewPassword
+        [Required]
+        [StringLength(
+            100,
+            ErrorMessage = "The {0} must be at least {2} characters long.",
+            MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
         public string Password
         {
             get;
